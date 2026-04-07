2934cab2adababc494ddc231982cbeacedfdcbae
theocaulet <caulet.theo@gmail.com>
2026-04-07 15:16:05 +0200

    Add asynchronous function to wait for a random delay

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module contains an asynchronous function that waits for a random delay
+between 0 and a specified maximum delay, then returns the actual delay.
+"""
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Waits for a random delay between 0 and max_delay seconds, then returns the
+    actual delay."""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
