307a60ed0d4189f001ecd636be570d90d223af76
theocaulet <caulet.theo@gmail.com>
2026-04-07 10:59:39 +0200

    Add module with annotated variables and their types

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains 4 variables that are annotated
+with their respective types.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
