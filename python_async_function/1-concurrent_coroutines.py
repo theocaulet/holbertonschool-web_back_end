90e16ac58b1a7d62e8843f978b2d3337391ea250
theocaulet <caulet.theo@gmail.com>
2026-04-07 15:48:28 +0200

    Add asynchronous function to return delays from multiple coroutines

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This module contains an asynchronous function that takes in two integers,
+n and max_delay, and returns a list of all the delays (float values)
+of the n coroutines created by wait_random(max_delay), in the order they are
+completed.
+"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list:
+    """Takes in two integers, n and max_delay, and returns a list of
+      all the delays (float values) of the n coroutines created by wait_random(max_delay),
+      in the order they are completed."""
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = []
+    for coroutine in asyncio.as_completed(tasks):
+        delay = await coroutine
+        delays.append(delay)
+    return delays
