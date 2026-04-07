9c5b9fbb39ada0a2068038ae8601661f0b644d8c
theocaulet <caulet.theo@gmail.com>
2026-04-07 11:10:36 +0200

    Add function to sum a list of floats and return the result

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains a function that sums a list of floats
+and returns the result as a float.
+"""
+import typing
+
+
+def sum_list(input_list: typing.List[float]) -> float:
+    """Sums a list of floats and return the result as a float."""
+    return sum(input_list)
