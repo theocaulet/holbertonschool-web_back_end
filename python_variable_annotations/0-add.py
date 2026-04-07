8a5a4715d1abc19edf393fa3334f93464a0fc57f
theocaulet <caulet.theo@gmail.com>
2026-04-07 10:21:32 +0200

    Add function to add two floats and return the result

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module contains a function that adds two floats 
+and returns the result as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Adds two floats and return the result as a float."""
+    return a + b
