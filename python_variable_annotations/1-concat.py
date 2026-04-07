bf6c4d80e6b1a8853b272af1cbea9b22c6a846cc
theocaulet <caulet.theo@gmail.com>
2026-04-07 10:30:35 +0200

    Add function to concatenate two strings and return the result

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module contains a function that concatenates two strings
+and returns the result as a string.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings and return the result as a string."""
+    return str1 + str2
