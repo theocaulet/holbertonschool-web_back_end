23e05a5f2c3119f665edc1057594bb91288121aa
theocaulet <caulet.theo@gmail.com>
2026-04-07 13:44:31 +0200

    Add function to create a multiplier that returns a callable for float multiplication

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module contains a function that takes a float multiplier as argument
+and returns a function that multiplies a float by multiplier.
+"""
+import typing
+
+
+def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
+    """Returns a function that multiplies a float by multiplier."""
+    def multiply(number: float) -> float:
+        """Returns the product of multiplier and number."""
+        return multiplier * number
+    return multiply
