2c96a88470853b5fdfbc0a85b419df4d34745382
theocaulet <caulet.theo@gmail.com>
2026-04-08 10:53:40 +0200

    Update module docstring and function docstring for measure_time

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+This module contains a function that measures the total execution time for
+wait_n(n, max_delay), and returns the average time per wait_random
+(n coroutines created by wait_random(max_delay)).
+"""
 
 import time
 import asyncio
@@ -6,6 +11,9 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
+    """Measures the total execution time for wait_n(n, max_delay), and
+    returns the average time per wait_random (n coroutines created by
+    wait_random(max_delay))."""
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
     end_time = time.time()
