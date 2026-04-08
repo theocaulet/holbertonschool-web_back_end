851cecc023dbc6d28ccb24688bde00ea152a04b4
theocaulet <caulet.theo@gmail.com>
2026-04-08 11:34:07 +0200

    Refactor async_generator to use typing module for type hinting

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,13 +7,13 @@ float between 0 and 10 every second for 10 seconds.
 
 import random
 import asyncio
-from typing import AsyncGenerator
+import typing
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> typing.AsyncGenerator[float, None]:
     """Yields a random float between 0 and 10 every second for
     10 seconds.
     """
     for _ in range(10):
         await asyncio.sleep(1)
-    yield random.uniform(0, 10)
+        yield random.uniform(0, 10)
