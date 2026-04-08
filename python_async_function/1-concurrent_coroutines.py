986eaa5e4464687ca83ff8ecc253e431c8aabfe6
theocaulet <caulet.theo@gmail.com>
2026-04-08 10:30:56 +0200

    Fix formatting in docstring for wait_n function

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -11,7 +11,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> list:
     """Takes in two integers, n and max_delay, and returns a list of
-      all the delays (float values) of the n coroutines created by wait_random(max_delay),
+      all the delays (float values) of the n coroutines created by
+      wait_random(max_delay),
       in the order they are completed."""
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = []
