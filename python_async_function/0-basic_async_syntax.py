1dff86ed230e1bc81740fcef11ec6181c21ac2dd
theocaulet <caulet.theo@gmail.com>
2026-04-08 10:29:57 +0200

    Fix formatting in docstring for wait_random function

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -8,8 +8,8 @@ import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Waits for a random delay between 0 and max_delay seconds, then returns the
-    actual delay."""
+    """Waits for a random delay between 0 and max_delay seconds, then returns
+    the actual delay."""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
