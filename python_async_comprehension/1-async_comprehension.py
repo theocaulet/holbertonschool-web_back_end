23598b84a131a69c883169ebf1d009e495439e27
theocaulet <caulet.theo@gmail.com>
2026-04-09 09:38:25 +0200

    Refactor async_comprehension to use typing.List for return type

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -6,10 +6,11 @@ and returns the 10 random numbers.
 """
 
 
+import typing
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list:
+async def async_comprehension() -> typing.List[float]:
     """Collects 10 random numbers using an async comprehensing over
     async_generator, and returns the 10 random numbers."""
     return [i async for i in async_generator()]
