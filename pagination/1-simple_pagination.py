3af4a647992e9c289e012961903bac2b23457e70
theocaulet <caulet.theo@gmail.com>
2026-04-20 15:42:11 +0200

    Add docstring to get_page method for clarity on return value

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -34,6 +34,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns the appropriate page of the dataset
+          (i.e. the correct list of rows)."""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
         start, end = index_range(page, page_size)
