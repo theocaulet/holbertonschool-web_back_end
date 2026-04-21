01200c0703b5e57927d0b74708f9b2aac4942f56
theocaulet <caulet.theo@gmail.com>
2026-04-21 15:23:02 +0200

    Refactor log_stats function in 12-log_stats.py for better structure and readability

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,14 +2,20 @@
 """Script that provides some stats about Nginx logs stored in MongoDB"""
 import pymongo
 
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client.logs
-collection = client.logs.nginx
 
-print("{} logs".format(db.nginx.count_documents({})))
-print("Methods:")
-for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-    count = db.nginx.count_documents({"method": method})
-    print("\tmethod {}: {}".format(method, count))
-status = db.nginx.count_documents({"method": "GET", "path": "/status"})
-print("{} status check".format(status))
+def log_stats():
+    """Returns some stats about Nginx logs stored in MongoDB"""
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client.logs
+
+    print("{} logs".format(db.nginx.count_documents({})))
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = db.nginx.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
+    status = db.nginx.count_documents({"method": "GET", "path": "/status"})
+    print("{} status check".format(status))
+
+
+if __name__ == "__main__":
+    log_stats()
