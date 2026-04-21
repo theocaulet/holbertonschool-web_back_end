a395574704ef9fb5e51eaf55bab35a2b9444e9e7
theocaulet <caulet.theo@gmail.com>
2026-04-21 13:21:53 +0200

    Add docstring to list_all function for better clarity

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
+""" 8-all """
 
 
 def list_all(mongo_collection):
+    """Lists all documents in the collection
+    Args:        mongo_collection: pymongo collection object
+    Returns:     list of documents in the collection
+    """
     return list(mongo_collection.find())
