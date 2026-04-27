baa95b0ab07781533252add71a224ef2cd376da4
theocaulet <caulet.theo@gmail.com>
2026-04-27 14:55:38 +0200

    Fix import statement and format getIncomeInEuros method

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,4 +1,4 @@
-import getBudgetObject from './7-getBudgetObject.js';
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
@@ -7,7 +7,7 @@ export default function getFullBudgetObject(income, gdp, capita) {
     getIncomeInDollars(income) {
       return `$${income}`;
     },
-    getIncomeInEuros(income){
+    getIncomeInEuros(income) {
       return `${income} euros`;
     },
   };
