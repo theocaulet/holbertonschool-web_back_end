960ccc9afc639343caa69d23bb1e5b7ba882582a
theocaulet <caulet.theo@gmail.com>
2026-05-04 11:20:45 +0200

    Add createInt8TypedArray function to create a typed array with specified length and value

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,10 @@
+export default function createInt8TypedArray(length, position, value) {
+    const buffer = new ArrayBuffer(length);
+    const data = new DataView(buffer);
+    if (position >= length) {
+        throw new Error('Position outside range');
+    } else {
+        data.setInt8(position, value);
+        return data;
+    }
+}
