f8d0e5858263e2871ad08f0ba16d8d8eeadd32da
theocaulet <caulet.theo@gmail.com>
2026-05-06 10:40:06 +0200

    Fix value handling in handleProfileSignup to ensure reason is converted to string

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,6 +4,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
         .then((results) => results.map(result => ({
             status: result.status,
-            value: result.value || result.reason,
+            value: result.value || result.reason.toString(),
         })));
 }
