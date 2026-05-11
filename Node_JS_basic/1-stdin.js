18a4cd02bf6f90e9702ddf1ce1f928bb3f61daa0
theocaulet <caulet.theo@gmail.com>
2026-05-11 10:59:35 +0200

    Add pause after user input in stdin.js to prevent immediate exit

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.toString().trim()}`);
+  process.stdin.pause();
 });
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
