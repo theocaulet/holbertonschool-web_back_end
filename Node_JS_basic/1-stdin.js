103f8265c26fad7c399fd3a82868d44b3db8c278
theocaulet <caulet.theo@gmail.com>
2026-05-11 10:51:11 +0200

    Add stdin.js to handle user input and display messages

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,7 @@
+console.log("Welcome to Holberton School, what is your name?");
+process.stdin.on("data", (data) => {
+    console.log(`Your name is: ${data.toString().trim()}`);
+})
+process.stdin.on("end", () => {
+    console.log("This important software is now closing");
+})
