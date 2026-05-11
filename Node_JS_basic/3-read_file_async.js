2b1dc6b5e96459bed4950bdb369a08db13b47d6b
theocaulet <caulet.theo@gmail.com>
2026-05-11 14:58:16 +0200

    Add countStudents function to read and group student data from a file

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const database = data.split('\n').filter((line) => line.trim() !== '').slice(1);
+        const groups = {};
+        database.forEach((line) => {
+          const parts = line.split(',');
+          const firstname = parts[0];
+          const field = parts[3];
+          if (groups[field]) {
+            groups[field].push(firstname);
+          } else {
+            groups[field] = [firstname];
+          }
+        });
+        console.log(`Number of students: ${database.length}`);
+        for (const field in groups) {
+          if (Object.prototype.hasOwnProperty.call(groups, field)) {
+            console.log(`Number of students in ${field}: ${groups[field].length}. List: ${groups[field].join(', ')}`);
+          }
+        }
+        resolve();
+      }
+    });
+  });
+}
+module.exports = countStudents;
